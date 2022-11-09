28bcc9b6f490a931acd228150166bf2b47d17703
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-11-09 12:31:04 +0300

    07.4 Two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  appTitle = 'Instagram'
-  text = ''
-
-  changeTitleHandler() {
-    this.appTitle = 'Facebook'
-  }
-
-  changeTextHandler(event: Event) {
-    this.text = (event.currentTarget as HTMLInputElement).value
-  }
+  text = 'start value'
 }
