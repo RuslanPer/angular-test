4d4e5b65ce4b5ad61bfcf90bde4077fbd8aafbd6
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-11-09 18:28:50 +0300

    09.5 NgSwitch

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,77 +1,10 @@
 import { Component } from '@angular/core'
 
-interface WeekGrade {
-  id: number
-  grade: number
-}
-
-interface Lesson {
-  id: number
-  title: string
-  weekGrades: WeekGrade[]
-}
-
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
-  isLoading = true
-  lessons: Lesson[] = [
-    {
-      id: 0,
-      title: 'Math',
-      weekGrades: [
-        {
-          id: 0,
-          grade: 5,
-        },
-        {
-          id: 1,
-          grade: 2,
-        },
-        {
-          id: 2,
-          grade: 4,
-        },
-        {
-          id: 3,
-          grade: 5,
-        },
-      ],
-    },
-    {
-      id: 1,
-      title: 'Physic',
-      weekGrades: [
-        {
-          id: 0,
-          grade: 3,
-        },
-        {
-          id: 1,
-          grade: 4,
-        },
-        {
-          id: 2,
-          grade: 4,
-        },
-        {
-          id: 3,
-          grade: 3,
-        },
-      ],
-    },
-  ]
-
-  constructor() {
-    setTimeout(() => {
-      this.isLoading = false
-    }, 3000)
-  }
-
-  getGrade(grade: string) {
-    // this.grades.push(grade)
-  }
+  value = ''
 }
