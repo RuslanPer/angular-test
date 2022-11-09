feb3f36d6e3fb6028091d3719c4e12638a5d1df0
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-11-09 17:10:11 +0300

    09.2 NgFor

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,14 +1,70 @@
 import { Component } from '@angular/core'
 
+interface WeekGrade {
+  id: number
+  grade: number
+}
+
+interface Lesson {
+  id: number
+  title: string
+  weekGrades: WeekGrade[]
+}
+
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
-  grades: string[] = ['math: 5', 'english: 3']
+  lessons: Lesson[] = [
+    {
+      id: 0,
+      title: 'Math',
+      weekGrades: [
+        {
+          id: 0,
+          grade: 5,
+        },
+        {
+          id: 1,
+          grade: 2,
+        },
+        {
+          id: 2,
+          grade: 4,
+        },
+        {
+          id: 3,
+          grade: 5,
+        },
+      ],
+    },
+    {
+      id: 1,
+      title: 'Physic',
+      weekGrades: [
+        {
+          id: 0,
+          grade: 3,
+        },
+        {
+          id: 1,
+          grade: 4,
+        },
+        {
+          id: 2,
+          grade: 4,
+        },
+        {
+          id: 3,
+          grade: 3,
+        },
+      ],
+    },
+  ]
 
   getGrade(grade: string) {
-    this.grades.push(grade)
+    // this.grades.push(grade)
   }
 }
