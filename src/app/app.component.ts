9e6be0828ad0ce6c365ac3d26d06ffbafd58d09d
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-11-09 12:22:14 +0300

    Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'test-app';
+  appTitle = 'Instagram'
+  text = ''
+
+  changeTitleHandler() {
+    this.appTitle = 'Facebook'
+  }
+
+  changeTextHandler(event: Event) {
+    this.text = (event.currentTarget as HTMLInputElement).value
+  }
 }
