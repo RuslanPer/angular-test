403f48e1449eb57933d9b7cf077b2cf1ff20e857
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-11-22 22:28:08 +0300

    14.05 Page not found redirect

--- src/app/app-routing-routing.module.ts
+++ src/app/app-routing-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './components/home/home.component'
 import { LoginComponent } from './components/login/login.component'
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
 import { ProfileComponent } from './components/profile/profile.component'
 import { TodosComponent } from './components/todos/todos.component'
 import { UsersComponent } from './components/users/users.component'
@@ -12,6 +13,8 @@ const routes: Routes = [
   { path: 'todos', component: TodosComponent },
   { path: 'users', component: UsersComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404' },
 ]
 
 @NgModule({
