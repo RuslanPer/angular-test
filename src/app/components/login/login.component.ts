ed21d063f496573460cd3d97118f817aeafebdc2
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-11-22 20:28:07 +0300

    13.4 Validators, Disabled, touched, dirty

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   loginForm = new FormGroup({
-    email: new FormControl(''),
+    email: new FormControl('', Validators.required),
     password: new FormControl(''),
   })
-  constructor() {}
 
-  ngOnInit(): void {}
+  get email() {
+    return this.loginForm.get('email')
+  }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
