ffb0a6837547090dff218a34e11b87a5cef060b5
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-11-22 22:08:06 +0300

    14.03 routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
-import { RouterModule } from '@angular/router'
 import { HomeComponent } from './components/home/home.component'
 import { LoginComponent } from './components/login/login.component'
 import { TodosComponent } from './components/todos/todos.component'
 import { UsersComponent } from './components/users/users.component'
 import { ProfileComponent } from './components/profile/profile.component'
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
+import { AppRoutingRoutingModule } from './app-routing-routing.module'
 
 @NgModule({
   declarations: [
@@ -20,16 +20,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     ProfileComponent,
     PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'login', component: LoginComponent },
-      { path: 'todos', component: TodosComponent },
-      { path: 'users', component: UsersComponent },
-      { path: 'profile', component: ProfileComponent },
-    ]),
-  ],
+  imports: [BrowserModule, AppRoutingRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
